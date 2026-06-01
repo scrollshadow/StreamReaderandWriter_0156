0632f2a1f8fbff4444d2e71ed8a0a1b312fa9efc
scrollshadow <zaldhypramana@gmail.com>
2026-06-01 15:30:06 +0700

    unli loop untuk menulis file

--- StreamReader&Writer.cpp
+++ StreamReader&Writer.cpp
@@ -9,4 +9,11 @@ int main()
     ofstream outfile;
     outfile.open("contohfile.txt");
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    while (true)
+    {
+        cout << "- ";//mendpdt setiap karakter dlm 1 baris
+        getline(cin, baris);//kondisi false jika baris hanya berisi karakter q
+        if (baris == "q") break;//menulis and memasukkan nilai varialbel baris kedlm file
+        outfile << baris << endl;
+    }
 }
