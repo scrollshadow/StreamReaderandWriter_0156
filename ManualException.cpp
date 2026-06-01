83fc800d78aba45f02a6e5e5cf2e3950b40815cc
scrollshadow <zaldhypramana@gmail.com>
2026-06-01 15:59:39 +0700

    kondisi jika trow yg bertipe data int

--- ManualException.cpp
+++ ManualException.cpp
@@ -9,9 +9,9 @@ int main()
         //throw 0,5; //melemparkan sebuah int maka
         cout << "pernyataan tidak dapat di atampilkan " << endl;
     }
-    catch(const std::exception& e)
+    catch(int a)
     {
-        std::cerr << e.what() << '\n';
+        cout << "pengecualian akan di eksekusi" << endl;
     }
     
 }
