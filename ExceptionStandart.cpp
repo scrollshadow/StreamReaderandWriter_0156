ada7f218c08bbcf71c0ba7a9c58dff105b83aece
scrollshadow <zaldhypramana@gmail.com>
2026-06-01 16:07:51 +0700

    last

---
+++ ExceptionStandart.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+int main()
+{
+    cout << "Awal program" << endl;//penanda 1
+    try
+    {
+        //deklarasi array data dg panjang index 3
+        array <int, 3> data = { 1, 2, 3 };
+        //memanggial array element ke 5 bararti akan error
+        cout << data.at(5) << endl;
+    }
+    //codisi jika eorr dlm try
+    catch(exception& e)
+    {
+        cout << e.what() << endl;
+    }
+    cout << "baris program terakir" << endl; //penanda 2
+    return 0;
+    
+}
