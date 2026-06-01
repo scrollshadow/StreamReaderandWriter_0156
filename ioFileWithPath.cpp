4ded9064f63800d795538d90f772d50695d6a9aa
scrollshadow <zaldhypramana@gmail.com>
2026-06-01 15:48:09 +0700

    menunjukan ke sebuah file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -7,6 +7,10 @@ int main()
 {
     string baris, Namafile;
     cout << "Masukan nama file : ";
-    cin >> Namafile:
+    cin >> Namafile;
+
+    ofstream outfile;
+    outfile.open(Namafile + ".txt", ios::out);
+    cout << ">= Menulis file, \'q\' Untuk kluar" << endl;
 
 }
