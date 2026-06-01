12af5bd0d6bb204c9f201616960db5f1b0d3b71a
scrollshadow <zaldhypramana@gmail.com>
2026-06-01 15:19:48 +0700

    membuat fungsi main

--- StreamReader&Writer.cpp
+++ StreamReader&Writer.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
+    string baris;
 
-}
+    ofstream outfile;
+    outfile.open("contohfile.txt");
