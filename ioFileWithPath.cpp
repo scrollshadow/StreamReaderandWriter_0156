e5b215ca5e6a50e35121eb801678205e46cebde3
scrollshadow <zaldhypramana@gmail.com>
2026-06-01 15:49:59 +0700

    looping manulis isi file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -13,4 +13,12 @@ int main()
     outfile.open(Namafile + ".txt", ios::out);
     cout << ">= Menulis file, \'q\' Untuk kluar" << endl;
 
+    while(true)
+    {
+        cout << "- ";//mendpdt setiap karakter dlm 1 baris
+        getline(cin, baris);//kondisi false jika baris hanya berisi karakter q
+        if (baris == "q") break;//menulis and memasukkan nilai varialbel baris kedlm file
+        outfile << baris << endl;
+    }
+
 }
